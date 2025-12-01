c1dd1fdf462964189888d75634f5c9caa6653a8c
OxyTheCrack <Gazy0906@outlook.com>
2025-12-01 16:21:21 +0800

    makefile usage

---
+++ CMake_Make_Makefile/demo.cpp
@@ -0,0 +1,8 @@
+#include "message.h"
+#include <iostream>
+int main()
+{
+    message();
+    std::cout << "Update 1" << std::endl;
+    return 0;
+}
---
+++ CMake_Make_Makefile/message.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "message.h"
+
+void message()
+{
+    std::cout << "This is a message from On The Radar Radio!" << std::endl;
+}
