22b8e4fe73f46531ce2b1d8c9f9cb7b3d98f655f
OxyTheCrack <Gazy0906@outlook.com>
2025-12-01 17:29:26 +0800

    Makefile Ended

---
+++ CMake_Make_Makefile/clean.cpp
@@ -0,0 +1,10 @@
+/*
+This is a test file for "make clean" operation to check if the ".PHONY: clean" work properly
+*/
+#include <iostream>
+
+int main()
+{
+    std::cout << "Test" << std::endl;
+    return 0;
+}
