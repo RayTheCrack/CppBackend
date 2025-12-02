478777332cb951996e806acd2741a39c576b3912
OxyTheCrack <Gazy0906@outlook.com>
2025-12-02 22:15:34 +0800

    gdb usage

---
+++ GNU_Debugger_Usage/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+void test()
+{
+
+}
+int i = 0;
+void test1()
+{
+	i++;
+}
+
+int main()
+{
+	while(true)
+	{
+		test();
+		test1();
+	}
+	return 0;
+}
+
+
---
+++ GNU_Debugger_Usage/main_error.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+
+//int divide(int x,int y)
+//{
+//	return x / y;
+//}
+
+
+
+int main()
+{
+    int* temp = nullptr;
+    *temp = 1; 
+    // std::cout << divide(100,0) << std::endl;
+    return 0;
+}
+
+
