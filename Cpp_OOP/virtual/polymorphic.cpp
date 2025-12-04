3f4c5cc9b47f8b724108a5f78ce15c04f659b010
OxyTheCrack <Gazy0906@outlook.com>
2025-12-04 20:06:50 +0800

    polymorphic

---
+++ Cpp_OOP/virtual/polymorphic.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Animal
+{
+    public:
+        virtual void speak() = 0; // Pure virtual function
+};
+
+class cat : public Animal
+{
+    public:
+        void speak() override
+        {
+            cout << "Meow" << endl;
+        }
+};
+
+class dog : public Animal
+{
+    public:
+        void speak() override
+        {
+            cout << "Woof" << endl;
+        }
+};
+
+void AnimalSound(Animal &animal)
+{
+    animal.speak();
+}
+
+int main()
+{
+    cat myCat;
+    dog myDog;
+
+    AnimalSound(myCat); // Outputs: Meow    
+    AnimalSound(myDog); // Outputs: Woof
+    
+    return 0;
+}
